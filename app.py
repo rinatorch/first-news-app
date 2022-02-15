31a553a6ff9cda56e74e0864682ab7f273041be5
rinatorch <64331817+rinatorch@users.noreply.github.com>
2022-02-14 21:53:57 -0500

    Created a detail page about each victim.

--- app.py
+++ app.py
@@ -17,6 +17,19 @@ def index():
     object_list = get_csv()
     return render_template(template, object_list=object_list)
 
+@app.route('/<row_id>/')
+def detail(row_id):
+    template = 'detail.html'
+    object_list = get_csv()
+    for row in object_list:
+        if row['id'] == row_id:
+            return render_template(template, object=row)
+
+def index():
+    template = 'index.html'
+    object_list = get_csv()
+    return render_template(template, object_list=object_list)
+
 if __name__ == '__main__':
     # Fire up the Flask test server
     app.run(debug=True, use_reloader=True)
